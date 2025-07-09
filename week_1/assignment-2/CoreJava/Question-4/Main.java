07e719fb9775417d4649fea669750da78037585a
AshishOdedra <ashishodedara2404@gmail.com>
2025-07-09 18:12:09 +0530

    Assignment 2 half done

--- week_1/assignment-2/CoreJava/Question-4/Main.java
+++ week_1/assignment-2/CoreJava/Question-4/Main.java
@@ -17,7 +17,7 @@ class Singleton{
     }
 }
 
-public class Singleton {
+public class Main {
     public static void main(String[] args) {
         Singleton s1 = Singleton.getInstance();
         Singleton s2 = Singleton.getInstance();
